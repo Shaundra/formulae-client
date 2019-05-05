77e27a04955b8548e1b2f3077bb108d2d87821e8
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-05-04 18:08:33 -0700

    Hookup NavBar buttons to link out to routes, WIP addFormula link

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ const App = () => {
       })
     }
   }
-
+// history={history}
   return (
     <Fragment>
       {/* {console.log('in render', formulaData)} */}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,55 @@
 import React, { Component, Fragment, useState, useEffect } from 'react';
 import { API_ROOT, HEADERS } from '../constants';
+// import { BrowserRouter as Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router';
+
 import { formatDate, fetchJWT, formatVidTime } from '../helpers'
 
 const NavBar = (props) => {
   const [isVisible, setIsVisible] = useState(false)
+  const [showForm, setShowForm] = useState(false)
 
   const handleClick = (ev) => {
-    console.log('clicked on')
+    switch (ev.target.name) {
+      case 'browse':
+        props.history.push('/browse')
+        break
+      case 'search':
+        props.history.push('/search')
+        break
+      case 'addFormula':
+        // close navbar, open FormulaForm as Modal (write a func, put it in the return)
+        // setShowForm(!showForm)
+        break
+      case 'allFormulae':
+        props.history.push('/formulae')
+        break
+    }
+
+    setIsVisible(!isVisible)
   }
+
   const openNav = () => {
     setIsVisible(!isVisible)
   }
+
+  // const renderFormulaForm = () => {
+  //   if (showForm) {
+  //     return (
+  //       <Modal
+  //         compToRender={
+  //           <FormulaForm
+  //             // parentID={props.elmt.id}
+  //             // contentType={props.elmt.content_type}
+  //             hideForm={handleFormClick}
+  //             allFormulae={allFormulae}
+  //             addFormula={setAllFormulae}
+  //           />
+  //         }
+  //       />
+  //     )
+  //   }
+  // }
 
   return (
     <Fragment>
@@ -19,10 +58,10 @@ const NavBar = (props) => {
       <div className='side-nav'>
         <button id='close-menu' onClick={openNav}>X</button>
         <h3>APP NAME / LOGO</h3>
-        <button className='nav-btn' onClick={handleClick}>BROWSE</button>
-        <button className='nav-btn' onClick={handleClick}>SEARCH</button>
-        <button className='nav-btn' onClick={handleClick}>CREATE FORMULA</button>
-        <button className='nav-btn' onClick={handleClick}>VIEW FORMULAE</button>
+        <button className='nav-btn' onClick={handleClick} name='browse'>BROWSE</button>
+        <button className='nav-btn' onClick={handleClick} name='search'>SEARCH</button>
+        <button className='nav-btn' onClick={handleClick} name='addFormula'>CREATE FORMULA</button>
+        <button className='nav-btn' onClick={handleClick} name='allFormulae'>VIEW FORMULAE</button>
         {/* <button className='nav-btn' onClick={handleClick}>ELEMENTS</button> */}
       </div>
     }
@@ -30,4 +69,4 @@ const NavBar = (props) => {
   )
 }
 
-export default NavBar
+export default withRouter(NavBar)
